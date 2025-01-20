b80c4e89d816d10ffb5caa5b0c63c0e9b16546f9
Jaqueline Rabelo <125192947+JaquelineRabelo@users.noreply.github.com>
2025-01-20 15:44:46 +0000

    Atualiza o submódulo CadastroClientesAPI

--- obj/Debug/net8.0/CadastroClientesAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/CadastroClientesAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CadastroClientesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2405b83527eacd021538b362121c21fc05c9da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+383e50ebe3a34164af0f24b87f510db4f5664e08")]
 [assembly: System.Reflection.AssemblyProductAttribute("CadastroClientesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CadastroClientesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
